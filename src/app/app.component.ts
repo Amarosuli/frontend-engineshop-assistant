e55478945636231d752ea03d9b717003c912398d
Amarosuli <vuvlnvw@gmail.com>
2024-02-15 21:51:37 +0700

    [update] Use LayoutComponent in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './shared/layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, LayoutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'frontend-engineshop-assistant';
+  title = 'engineshop assistant';
 }
