7c29f66c1198b9c48858a62d3195da29fd3978ac
Amarosuli <vuvlnvw@gmail.com>
2024-02-15 20:50:23 +0700

    [add] Lucide Angular to Config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,35 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {
+  DatabaseZap,
+  FileCog,
+  FlaskRound,
+  LogIn,
+  LucideAngularModule,
+  Map,
+  MonitorDot,
+  PackageCheck,
+  TableProperties,
+  Workflow,
+} from 'lucide-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      LucideAngularModule.pick({
+        Map,
+        FileCog,
+        Workflow,
+        FlaskRound,
+        MonitorDot,
+        DatabaseZap,
+        PackageCheck,
+        TableProperties,
+        LogIn
+      })
+    ),
+  ],
 };
