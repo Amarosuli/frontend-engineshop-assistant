60bedb44e92b424ab8126168a7e66e9be181a953
Amarosuli <vuvlnvw@gmail.com>
2024-02-15 19:29:15 +0700

    [add] fontSize

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+   content: ["./src/**/*.{html,ts}"],
+   theme: {
+      extend: {
+         fontSize: {
+            xxxs: ['8px', '10px'],
+            xxs: ['10px', '14px'],
+            '3xl': ['48px', '64px'],
+            '4xl': ['82px', '120px'],
+         }
+      },
+   },
+   plugins: [],
 }
 
