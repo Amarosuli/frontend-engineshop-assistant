2417830a156b3a56df20757682c0174ddeea7ef0
Amarosuli <vuvlnvw@gmail.com>
2024-02-15 21:53:38 +0700

    [add] List All Dashboard Menu

---
+++ src/app/constant/dashboard-menu.ts
@@ -0,0 +1,64 @@
+export const app = [
+  {
+    id: 0,
+    title: 'Engine In Out',
+    href: '/engine-in-out',
+    icon: 'workflow',
+  },
+  {
+    id: 1,
+    title: 'Preservation Control',
+    href: '/preservation-control',
+    icon: 'monitor-dot',
+  },
+  {
+    id: 2,
+    title: 'Engine Location',
+    href: '/engine-location',
+    icon: 'map',
+  },
+  {
+    id: 3,
+    title: 'Surat Pengiriman',
+    href: '/surat-pengiriman',
+    icon: 'package-check',
+  },
+];
+export const manage = [
+  {
+    id: 0,
+    title: 'Engine List',
+    href: '/engine-list',
+    icon: 'table-properties',
+  },
+  {
+    id: 1,
+    title: 'Engine Model',
+    href: '/engine-model',
+    icon: 'database-zap',
+  },
+  {
+    id: 2,
+    title: 'Engine Family',
+    href: '/engine-family',
+    icon: 'database-zap',
+  },
+  {
+    id: 3,
+    title: 'Customer',
+    href: '/customer',
+    icon: 'database-zap',
+  },
+  {
+    id: 4,
+    title: 'PDF Template',
+    href: '/login',
+    icon: 'file-cog',
+  },
+  {
+    id: 5,
+    title: 'Development Page',
+    href: '/login',
+    icon: 'flask-round',
+  },
+];
