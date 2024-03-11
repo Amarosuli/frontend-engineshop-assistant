7ad995d76dd702ec866b6a369fe7b36cf87e00d3
Amarosuli <vuvlnvw@gmail.com>
2024-03-11 11:10:07 +0700

    [update] Remove unused import

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { app, manage } from '../../constant/dashboard-menu';
-import { LucideAngularComponent, LucideAngularModule } from 'lucide-angular';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-home',
