90bfad5646fff3ea0018c700f3f5b480dd585fb1
Amarosuli <vuvlnvw@gmail.com>
2024-03-20 10:18:15 +0700

    [update] Add route manage/customer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { CustomerComponent } from './pages/customer/customer.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'manage/customer',
+    component: CustomerComponent,
   },
   {
     path: '**',
