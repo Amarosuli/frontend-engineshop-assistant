2797a152a04c5c142a9af410d703d5e950c35e0b
Amarosuli <vuvlnvw@gmail.com>
2024-04-02 07:26:22 +0700

    [update] Add totalPage properties to ApiResponse type

--- src/app/type/Api.ts
+++ src/app/type/Api.ts
@@ -1,34 +1,35 @@
 import { z } from 'zod';
 
 export type ApiResponse<T> = {
-  body: {
-    data: T;
-    currentPage: number;
-    hasNext: boolean;
-    hasPrev: boolean;
-    lastPage: boolean;
-  };
-  message: string;
-  meta: string;
-  time: string;
+	body: {
+		data: T;
+		currentPage: number;
+		totalPage: number;
+		hasNext: boolean;
+		hasPrev: boolean;
+		lastPage: boolean;
+	};
+	message: string;
+	meta: string;
+	time: string;
 };
 
 export type Params = {
-  totalItemsPerPage?: number;
-  currentPage?: number | 0;
+	totalItemsPerPage?: number;
+	currentPage?: number | 0;
 };
 
 export function ZodApiResponse<T extends z.ZodTypeAny>(data: T) {
-  return z.object({
-    body: z.object({
-      data: z.array(data),
-      currentPage: z.number(),
-      hasNext: z.boolean(),
-      hasPrev: z.boolean(),
-      lastPage: z.boolean(),
-    }),
-    message: z.string(),
-    meta: z.string(),
-    time: z.string(),
-  });
+	return z.object({
+		body: z.object({
+			data: z.array(data),
+			currentPage: z.number(),
+			hasNext: z.boolean(),
+			hasPrev: z.boolean(),
+			lastPage: z.boolean()
+		}),
+		message: z.string(),
+		meta: z.string(),
+		time: z.string()
+	});
 }
