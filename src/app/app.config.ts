cc3b200b196a3f1f38f31e5d6af28a0a5b098b24
Amarosuli <vuvlnvw@gmail.com>
2024-04-02 07:25:14 +0700

    [update] Add icons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,40 +4,52 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import {
-   ArrowDown,
-  ArrowUp,
-  DatabaseZap,
-  FileCog,
-  FlaskRound,
-  LogIn,
-  LucideAngularModule,
-  Map,
-  MonitorDot,
-  PackageCheck,
-  TableProperties,
-  Workflow,
+	ArrowDown,
+	ArrowUp,
+	DatabaseZap,
+	FileCog,
+	FlaskRound,
+	LogIn,
+	LucideAngularModule,
+	Map,
+	MonitorDot,
+	PackageCheck,
+	TableProperties,
+	Workflow,
+	LayoutGrid,
+	SlidersHorizontal,
+	Plus,
+	FilterX,
+	ChevronRight,
+	ChevronLeft
 } from 'lucide-angular';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideAnimations(),
-    provideRouter(routes),
-    provideHttpClient(),
-    importProvidersFrom(
-      LucideAngularModule.pick({
-        Map,
-        FileCog,
-        Workflow,
-        FlaskRound,
-        MonitorDot,
-        DatabaseZap,
-        PackageCheck,
-        TableProperties,
-        LogIn,
-        ArrowUp,
-        ArrowDown
-      })
-    ),
-  ],
+	providers: [
+		provideAnimations(),
+		provideRouter(routes),
+		provideHttpClient(),
+		importProvidersFrom(
+			LucideAngularModule.pick({
+				Map,
+				FileCog,
+				Workflow,
+				FlaskRound,
+				MonitorDot,
+				DatabaseZap,
+				PackageCheck,
+				TableProperties,
+				LogIn,
+				ArrowUp,
+				ArrowDown,
+				LayoutGrid,
+				FilterX,
+				SlidersHorizontal,
+				Plus,
+				ChevronLeft,
+				ChevronRight
+			})
+		)
+	]
 };
